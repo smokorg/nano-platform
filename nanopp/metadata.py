3dbac664f7962e3de6ec82289981da9acebcce4e
Pavle Jonoski <jonoski.pavle@gmail.com>
2014-10-26 14:19:19 +0100

    Setup basic python project skeleton.

---
+++ nanopp/metadata.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""Project metadata
+
+Information describing the project.
+"""
+
+# The package name, which is also the "UNIX name" for the project.
+package = 'nanopp'
+project = "Nano Plugins Platform"
+project_no_spaces = project.replace(' ', '')
+version = '0.1'
+description = 'It does cool things'
+authors = ['Pavle Jonoski']
+authors_string = ', '.join(authors)
+emails = ['jonoski.pavle@gmail.com']
+license = 'MIT'
+copyright = '2014 ' + authors_string
+url = 'http://xmpp-server-project.org/nano-plugins-platform'
