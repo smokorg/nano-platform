2d0527a5fccaba698065e2863b1fd1657d19d127
Pavle Jonoski <jonoski.pavle@gmail.com>
2014-10-26 18:57:35 +0100

    Added a bit of documentation on the Plugin class.

--- nanopp/platform.py
+++ nanopp/platform.py
@@ -1,11 +1,47 @@
 
 
 class Plugin:
+    """ Base class for all Plugins.
+    
+    This is the entry point in the plugin itself. Each plugin MUST exponse 
+    at most one implementation of Plguin.
+    
+    """
+    
     STATE_UNINSTALLED = 0x0
+    """ The plugin is loaded but the installation has not yet taken place.
+    """
+    
     STATE_INSTALLED = 0x1
+    """The plugin is installed on the platform.
+    
+    At this point all dependencis for the plugin had been resolved and satisfyed.
+    """
+    
     STATE_ACTIVE = 0x2
+    """ The plugin has been successfully activated.
+    
+    At this point, the call to Plugin.activate(...) has been made and no error 
+    were detected.
+    """
+    
     STATE_DEACTIVATED = 0x3
+    """ The plugin has been deactivated, but it is still available on the platorm.
+    """
+    
     STATE_DISPOSED = 0x4
+    """ The plugin is ready to be removed from the platform.
+    
+    This point may have been reached by calling PluginManager.dispose(...) or
+    it may be a result of an error during installtion (such as dependencies that
+    had not been satisfyed).
+    
+    The plugin will be completel removed from the platform on the next garbadge 
+    collection cycle.
+    """
+    
+    
+    
     def activate(self):
         pass
     
