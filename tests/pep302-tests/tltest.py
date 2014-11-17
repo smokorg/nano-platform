75932cbd7ac3010d511cee286a1afee3ea56a60a
Pavle Jonoski <jonoski.pavle@gmail.com>
2014-11-17 19:59:31 +0100

    Added test in pep-302

--- tests/pep302-tests/tltest.py
+++ tests/pep302-tests/tltest.py
@@ -1,7 +1,13 @@
+real_import = __import__
+
+def test_import(*args, **kwargs):
+    print('test import-ot')
+    return real_import(*args, **kwargs)
+
+__import__ = test_import    
+
 import tmod.test_pep302
 print('Will try to import now...')
 import sys
 print(sys.meta_path)
 tmod.test_pep302.perform_302_test()
-
-import lajno
