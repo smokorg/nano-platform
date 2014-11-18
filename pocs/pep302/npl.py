b17408c0ee6bbcca3745fd44c7b57d6af71ff97f
Pavle Jonoski <jonoski.pavle@gmail.com>
2014-11-18 23:54:28 +0100

    Completed the PEP-302 POCs.

--- pocs/pep302/npl.py
+++ pocs/pep302/npl.py
@@ -12,9 +12,10 @@ import sys
 
 class NPLoader(SourceLoader):
 
-    def __init__(self, context, packages=[]):
+    def __init__(self, context, packages=[], restricted=[]):
         self.context = context
         self.packages = packages
+        self.restricted = restricted
 
     def get_filename(self, fullname):
         cwd = os.getcwd()
@@ -31,11 +32,19 @@ class NPLoader(SourceLoader):
         return ""
 
     def exec_module(self, module):
-        print("Exec module: %s" % module)
+        print(" > exec_module: %s" % module)
         module.__context__ = self.context
         module.__dict__["__import__"] = context_aware_import_factory(self.context)
         return SourceLoader.exec_module(self, module)
 
+    def get_code(self, fullname):
+        self.check_if_restricted(fullname)
+        return SourceLoader.get_code(self, fullname)
+
+    def check_if_restricted(self, fullname):
+        if fullname in self.restricted:
+            print(' !!! Restricted: %s' % fullname)
+            raise ImportError('%s: loading restricted' % fullname, name=fullname)
 
 class NPFinder:
 
@@ -43,19 +52,23 @@ class NPFinder:
         self.tldata = tldata
         self.packages = ['testpkg', 'testpkg.subpkg', 'testpkg.subpkg2']
         self.modules = ['testpkg.testmod', 'testpkg.subpkg.testsubmod', 'testpkg.subpkg2.testsubmod2']
+        self.restricted = ['testpkg.subpkg2', 'testpkg.subpkg.testsubmod']
 
     def find_module(self, fullname, path=None):
-        print("NPFilnderLoader->find_module(fullname=%s, path=%s)"%( fullname, path))
+        print('import %s (%s)'%(fullname, path))
         self.print_curr_context()
         if fullname in self.packages or fullname in self.modules:
-            return NPLoader(context=fullname, packages=self.packages)
-        # we handle all imports
+            print(' > known module/package')
+            return NPLoader(context=fullname, packages=self.packages, restricted=self.restricted)
+        else:
+            print(' > Not known to this finder')
         return None
+
     def print_curr_context(self):
         try:
-            print('    -> Current Context: %s' % self.tldata.context)
+            print(' > Imported by: %s' % self.tldata.context)
         except:
-            print("    -> No context set")
+            print("    -> Top level import (context not set)")
 
 # keep the reference to the original import
 original_import = __import__
@@ -63,7 +76,7 @@ tl = threading.local()
 
 def context_aware_import_factory(context):
     def context_aware_import(*args, **kwargs):
-        print("context_aware_import() args=%s; kwargs=%s"%(args, kwargs))
+        print("context_aware_import() args=%s; kwargs=%s" % (args, kwargs))
         tl.context = context
         return original_import(*args, **kwargs)
     return context_aware_import
@@ -73,4 +86,11 @@ sys.meta_path.insert(0, NPFinder(tldata=tl))
 
 # let's try to import something:
 
+print(" --- LOADING CUSTOM MODULE ---")
 import testpkg.testmod
+print(" -----------------------------")
+
+print(' --- LOADING RESTRICTED --- ')
+# try loading restricted package
+
+import testpkg.subpkg.testsubmod
