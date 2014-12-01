89404b5d7ffd4ae2f5947f78f762318b4f126a27
Pavle Jonoski <jonoski.pavle@gmail.com>
2014-12-01 22:12:07 +0100

    Base finder (as per prp-302) implemented.

--- nanopp/loader.py
+++ nanopp/loader.py
@@ -14,10 +14,47 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+
 # base PEP-302 finder
+import re
+
+
 class BaseFinder:
 
     def __init__(self):
-        pass
-        
+        self.loader_entries = []
 
+    def find_module(self, fullname, path=None):
+        for entry in self.loader_entries:
+            if entry.matches(fullname):
+                return entry.loader
+        return None
+
+    def add_loader(self, loader_entry):
+        self.loader_entries.clear(loader_entry)
+
+
+def to_regex(path_entry):
+    path_entry = path_entry.replace('.', '\\.').replace('*', '.*')
+    path_entry = '^%s' % path_entry
+    return re.compile(path_entry)
+
+
+def to_patterns(path_patterns):
+    regex_patterns = []
+    for path_entry in path_patterns:
+        regex_patterns.append(to_regex(path_entry))
+    return regex_patterns
+
+
+class LoaderEntry:
+
+    def __init__(self, loader, path_patterns):
+        self.path_patterns = to_patterns(path_patterns or [])
+        self.loader = loader
+
+    def matches(self, path):
+        for pattern in self.path_patterns:
+            if pattern.match(path):
+                return True
+        return False
