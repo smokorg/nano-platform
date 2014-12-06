ab37bf5efa2eff775006a10c4750e5dc875ab6f7
Pavle Jonoski <jonoski.pavle@gmail.com>
2014-12-06 20:58:12 +0100

    Plugin Manifest Parser

--- nanopp/plugins/support.py
+++ nanopp/plugins/support.py
@@ -1,6 +1,23 @@
-import logging
+#    This file is part of Nano Plugins Platform
+#    Copyright (C) 2014 Pavle Jonoski
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 __author__ = 'pavle'
+
+import logging
+import re
 
 """
 Plugin structure:
@@ -12,6 +29,7 @@ PLUGIN.MF structure:
 
 Pligin-Id: <plugin_id> - string, must be unique
 Version: <major>.<minor>.<patch>
+Plugin-Classes: <fully-qualified-classname>,...
 Requires: <RequiresEntrySpec>,...
 Exports: <ExportsEntrySpec>,...
 Requires-Plugins: <RequiresEntrySpec>,...
@@ -20,6 +38,7 @@ Example:
 
 Plugin-Id: nanopp.interactive-shell
 Version: 0.1.1
+Plugin-Classes: nanopp.interactive.ShellPlugin,nanopp.interactive.TelnetPlugin
 Requires: nanopp.* [0.0.1, 1.0);
 Exports: nanopp.remote.*(1.0.0); nanopp.tools(1.0.0)
 Requires-Plugins: nanopp.network-support
@@ -109,7 +128,11 @@ class RequiresEntry(Entry):
 class PluginManifest:
 
     def __init__(self):
+        self.id = None
+        self.version = None
+        self.plugin_classes = []
         self.requires = []
+        self.requires_plugins = []
         self.exports = []
 
 
@@ -123,3 +146,82 @@ class PluginResource:
 
     def read_resource(self, resource_path):
         pass
+
+
+class PluginManifestParser:
+
+    RGX_PLUGIN_ENTRY = "^(?P<block>[^:]+):(?P<content>.*)"
+    BLOCKS = {
+        "PLUGIN-ID": {
+            "content_handler": "read_plugin_id"
+        },
+        "VERSION": {
+            "content_handler": "read_plugin_version"
+        },
+        "PLUGIN-CLASSES": {
+            "content_handler": "read_plugin_classes"
+        },
+        "REQUIRES": {
+            "content_handler": "read_requires"
+        },
+        "EXPORTS": {
+            "content_handler": "read_exports"
+        },
+        "REQUIRES-PLUGINS": {
+            "content_handler": "read_requires_plugins"
+        }
+    }
+
+    def __init__(self, comment=''):
+        self.comment = comment
+
+    def parse(self, manifest_stream):
+        while True:
+            line = manifest_stream.readline()
+            if line is None:
+                break
+            line = line.strip()
+            if line[0] == self.comment:
+                continue
+
+    def read_block(self, block, content):
+        block_config = PluginManifestParser.BLOCKS.get(block.upper())
+        if block_config:
+            read_method = getattr(self, block_config['content_handler'])
+            return read_method(content)
+        return self.read_unknown_block(block, content)
+
+    def read_plugin_id(self, content):
+        pass
+
+    def read_plugin_version(self, content):
+        pass
+
+    def read_plugin_classes(self, content):
+        pass
+
+    def read_requires(self, content):
+        pass
+
+    def read_exports(self, content):
+        pass
+
+    def read_requires_plugins(self, content):
+        pass
+
+    def read_unknown_block(self, block, content):
+        logger.warn("Unknown block [%s] in manifest. Content: %s" % (block, content))
+
+
+if __name__ == '__main__':
+    RGX_PLUGIN_ENTRY = "^(?P<block>[^:]+):(?P<content>.*)"
+
+    rg = re.compile(RGX_PLUGIN_ENTRY)
+
+    m = rg.match("Plugin-Id: The content")
+
+    if m is None:
+        print("No match")
+    else:
+        print("Block: ", m.group("block"))
+        print("Content: ", m.group("content"))
