89c55e5bccdf04aa73464758b8eb5ca14cbe39b9
Pavle Jonoski <jonoski.pavle@gmail.com>
2015-05-12 21:51:44 +0200

    Fixed unit test

--- tests/test_dependencies.py
+++ tests/test_dependencies.py
@@ -1,28 +1,14 @@
 from logging import DEBUG
 import logging
 import sys
+sys.path.append("..")
 from unittest.case import TestCase
-from termite.dependencies import DependenciesManager, Graph, Vertex, PluginDependenciesManager
+from termite.dependencies import Graph, Vertex, PluginDependenciesManager
 
 __author__ = 'pavle'
 
-sys.path.append("../..")
+
 logging.basicConfig(level=DEBUG)
-
-
-class TestDependencyManager(TestCase):
-
-    def test_create_dependency_graph(self):
-        dm = DependenciesManager()
-        dm.add_dependency('A', ['B', 'C'])
-        dm.add_dependency('B', [])
-        dm.add_dependency('C', ['D', 'E', 'F'])
-        dm.add_dependency('D', ['F'])
-        dm.add_dependency('E', ['B'])
-        dm.add_dependency('F', ['E'])
-
-        dependency_order = dm.get_install_order()
-        self.assertEqual(['B', 'E', 'F', 'D', 'C', 'A'], dependency_order)
 
 
 class TestGraph(TestCase):
