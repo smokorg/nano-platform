b03645b1b2cfabd743803e551086be9ef91b8751
Pavle Jonoski <pavle.jonoski@gmail.com>
2016-03-22 00:13:24 +0100

    Added documentation for the tools.Proxy type.

--- termite/tools.py
+++ termite/tools.py
@@ -2,6 +2,25 @@ __author__ = 'pavle'
 
 
 class Proxy:
+    """Proxy wrapper that adds facilities for intercepting inerations.
+    
+    Wraps the original (target) object and exposes its methods and properties as
+    its own. This is achieved by overriding the __getattribute__ native method.
+    When a property is requested (reading/setting a value to a property, calling a
+    method) on the proxy object, the wrapped object is inspected and a convinient
+    wrapper is returned back to the requestor.
+    For the requesting party, the proxy object follows the same interface as the 
+    original object type. Some special methods that belong specifically to the 
+    Proxy type are black-listed and will not be returned if requested.
+    
+    There are three extension points defined by the proxy that add facilities for
+    interception of the interaction:
+    
+     - on_method_call - called every time a method is called and it exists in the 
+        original object
+     - on_property - called when a property is requested (to be read or written to)
+     - on_missing - called when the actual method does not exist
+    """
 
     NATIVE_METHODS = ['target', '__method_call_wrapper__', 'on_method_call', 'on_property', 'on_missing' ,'__init__']
 
